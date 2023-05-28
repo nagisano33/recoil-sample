7d87e7446c598d95c6483bd21acdffbf948cb11a
nagisano33 <vita.stars2@gmail.com>
2023-05-29 01:21:12 +0900

    Update 状態によってページの見た目を切り替える

--- src/MyPage/MyPage.tsx
+++ src/MyPage/MyPage.tsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { Control } from "./components/Control";
 import { Introduction } from "./components/Introduction";
+import styled from "styled-components";
+import { useRecoilValue } from "recoil";
+import { ThemeCss } from "../contexts/themeCss";
+
+const StyledDiv = styled.div`
+  height: 100vh;
+  background: ${(props) => props.theme.background};
+  color: ${(props) => props.theme.fontColor};
+`;
 
 /**
  * ページ
  * @returns 関数コンポーネント
  */
 export const MyPage = () => {
+  const themeCss = useRecoilValue(ThemeCss);
   return (
-    <div>
+    <StyledDiv theme={themeCss}>
       MyPage
       <div>
         <Control />
@@ -16,6 +26,6 @@ export const MyPage = () => {
       <div>
         <Introduction />
       </div>
-    </div>
+    </StyledDiv>
   );
 };
