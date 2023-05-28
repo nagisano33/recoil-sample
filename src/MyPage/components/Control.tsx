7169ab1ba4531787827399d130b37e5bf23ffa19
nagisano33 <vita.stars2@gmail.com>
2023-05-29 01:20:34 +0900

    Update テーマを切り替えるボタンを設置

--- src/MyPage/components/Control.tsx
+++ src/MyPage/components/Control.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from "react";
+import { DarkModeButton } from "./DarkModeButton";
+import { LightModeButton } from "./LightModeButton";
+import { useRecoilValue } from "recoil";
+import { themeState } from "../../contexts/themeState";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  margin: 1em;
+`;
 
 export const Control = () => {
+  const themeMode = useRecoilValue(themeState);
   return (
-    <div>Control</div>
-  )
-}
+    <StyledDiv>
+      {themeMode === "dark" && <LightModeButton />}
+      {themeMode === "light" && <DarkModeButton />}
+    </StyledDiv>
+  );
+};
