614d3cf548ded61d332f42bd210a9f8601e08477
nagisano33 <vita.stars2@gmail.com>
2023-06-12 01:16:48 +0900

    Update 行ヘッダーテーブルテスト表示

--- src/MyPage/MyPage.tsx
+++ src/MyPage/MyPage.tsx
@@ -4,12 +4,23 @@ import { Introduction } from "./components/Introduction";
 import styled from "styled-components";
 import { useRecoilValue } from "recoil";
 import { ThemeCss } from "../contexts/themeCss";
+import {
+  IDisplayable,
+  RowHeaderTable,
+} from "../components/MyTable/RowHeaderTable";
 
 const StyledDiv = styled.div`
   height: 100vh;
   background: ${(props) => props.theme.background};
   color: ${(props) => props.theme.fontColor};
 `;
+
+interface ITest extends IDisplayable {
+  id: number;
+  data: string[];
+  name: string;
+  description?: string;
+}
 
 /**
  * ページ
@@ -25,6 +36,19 @@ export const MyPage = () => {
       </div>
       <div>
         <Introduction />
+      </div>
+      <div style={{ width: "40%" }}>
+        <RowHeaderTable<ITest>
+          headerType={"row"}
+          // ここに渡す順番で表示が変わる
+          rows={{ id: 1, data: ["hoge", "fuga", "piyo"], name: "hoge" }}
+          titles={{
+            id: "ID",
+            name: "名前",
+            data: "データ",
+            description: "説明",
+          }}
+        />
       </div>
     </StyledDiv>
   );
