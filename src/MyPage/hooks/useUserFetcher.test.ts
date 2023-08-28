7250d3d734a1fce030df590dfd4e4e0bcef03484
nagisano33 <vita.stars2@gmail.com>
2023-08-29 01:38:21 +0900

    update useUserFetcher.ts

--- src/MyPage/hooks/useUserFetcher.test.ts
+++ src/MyPage/hooks/useUserFetcher.test.ts
@@ -1,8 +1,11 @@
+import axios from "axios";
 import { renderHook } from "@testing-library/react";
 import { useUserFetcher } from "./useUserFetcher";
 
 describe("useUserFetcher", () => {
-  test("username: admin が返る", async () => {
+
+  test("axios#get を実行する", async () => {
+    jest.spyOn(axios, "get").mockResolvedValue({ data: { username: "admin" } });
     const { result: userFetcher } = renderHook(() => useUserFetcher());
 
     const result = await userFetcher.current.execute();
